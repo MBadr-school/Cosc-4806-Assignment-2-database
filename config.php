b811931ff24f5abb1eeaee858d6188f04e347e86
MBadr-school <mbadr@algomau.ca>
2025-05-30 15:26:39 +0000

    config.php updated

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define( 'DB_HOST', 'xxw.h.filess.io');
+define( 'DB_USER', '4806_sentenceby');
+define( 'DB_PASS', $_ENV[ 'DB_PASS']);
+define( 'DB_DATABASE', '4806_sentenceby');
+define( 'DB_PORT', '3305');
+
+?>
