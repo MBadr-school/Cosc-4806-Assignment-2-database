bdffbebb38b88baa52e95160ba196e69937c02ab
MBadr-school <mbadr@algomau.ca>
2025-05-30 21:15:53 +0000

    updated index.php to test user creation, login, and display all users

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
-
 <?php
 
-require_once ('user.php');
+require_once('user.php');
 
 $user = new User();
-$user_list = $user->get_all_users();
 
+//test creat users
+$result = $user->create_user("TestUser", "Password12345");
+echo $result . "<br>";
+
+//test login
+$login = $user->login("TestUser", "Password12345");
+if ($login) {
+    echo "Login successful.";
+} else {
+    echo "Invalid login.";
+}
+
+//display all users
 echo "<pre>";
-print_r ($user_list);
+print_r($user->get_all_users());
+
+?>
