bf8b917b16420fb034dcd8d500865fc47d37b689
MBadr-school <mbadr@algomau.ca>
2025-05-30 20:43:06 +0000

    FIX

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'DB_HOST', 'COSC4806_anywherein');
+define( 'DB_HOST', 'hyek7.h.filess.io');
 define( 'DB_USER', 'COSC4806_anywherein');
 define( 'DB_PASS', $_ENV[ 'DB_PASS']);
 define( 'DB_DATABASE', 'COSC4806_anywherein');
