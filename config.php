8ab6e43bff8040d4c0a6e566a9e28ea9890bb308
MBadr-school <mbadr@algomau.ca>
2025-05-30 16:19:12 +0000

    updated the config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 define( 'DB_HOST', 'eqo7q.h.filess.io');
-define( 'DB_USER', '4806_sentenceby');
+define( 'DB_USER', 'cosc4806_flydoesmud');
 define( 'DB_PASS', $_ENV[ 'DB_PASS']);
-define( 'DB_DATABASE', '4806_sentenceby');
+define( 'DB_DATABASE', 'cosc4806_flydoesmud');
 define( 'DB_PORT', '3305');
 
 ?>
