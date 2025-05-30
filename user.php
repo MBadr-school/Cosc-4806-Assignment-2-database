ed79e143ff094b80064c0aeb9fb670e2136ba64b
MBadr-school <mbadr@algomau.ca>
2025-05-30 16:25:47 +0000

    user.php fix

--- user.php
+++ user.php
@@ -1,15 +1,16 @@
-<?php
+  <?php
 
-require_once ('database.php' );
+  require_once('database.php');
 
-Class User {
-  
-public function get_all_users () {
-$db = db_connect( );
-$statement = $db->prepare("select * from users;");
-$statement-›execute();
-$rows = $statement->fetch(PDO:: FETCH_ASSOC);
-return $rows;
-｝
+  class User {
+
+      public function get_all_users() {
+          $db = db_connect();
+          $statement = $db->prepare("SELECT * FROM users;");
+          $statement->execute();
+          $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+          return $rows;
+      }
+
   }
-?>
+  ?>
